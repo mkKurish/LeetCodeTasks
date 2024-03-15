0fa4924fbfa5922e84de298b660d18eeb4ee42a0
mkKurish <kurish04@mail.ru>
2024-03-15 13:23:41 +0300

    daily task 15

--- 238. Product of Array Except Self/Product of Array Except Self js.js
+++ 238. Product of Array Except Self/Product of Array Except Self js.js
@@ -13,3 +13,18 @@ var productExceptSelf = function(nums) {
     })
     return answer
 };
+
+var productExceptSelf = function(nums) {
+    const result = Array(nums.length)
+    result[0] = 1
+    for (let i = 1; i < nums.length; i++){
+        result[i] = result[i-1] * nums[i-1]
+    }
+    const reversed = Array(nums.length)
+    reversed[nums.length - 1] = 1
+    for (let i = nums.length - 2; i >= 0; i--){
+        reversed[i] = reversed[i+1] * nums[i+1]
+        result[i] *= reversed[i]
+    }
+    return result
+};
